bdd0db8a03d0f01a679d1f5e5e39616943560e29
andrea <andrea.portscher@student.uibk.ac.at>
2021-10-19 14:08:33 +0200

    Move headers to header folder

--- headers/Game.h
+++ headers/Game.h
@@ -1,11 +1,7 @@
 #ifndef RPG_GAME_H
 #define RPG_GAME_H
 
-#include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Audio.hpp>
-#include <SFML/Network.hpp>
+#include "State.h"
 
 /*
  * Wrapper class acting as game engine.
@@ -14,12 +10,15 @@ class Game
 {
 private:
     void initVariables();
-
     void initWindow();
 
     sf::RenderWindow *window; // dynamically allocated so that it can be deleted
     sf::VideoMode videoMode;
     sf::Event event;
+
+    // Time variables
+    sf::Clock dtClock;
+    float dt; // time delta
 public:
     // Constructor
     Game();
@@ -31,9 +30,13 @@ public:
     // Everything defining behind-the-scenes logic
     void update();
 
+    // Update time variable dt (new time is the time it takes to update and render 1 frame)
+    void updateDT();
+
     // Visual representation of the game
     void render();
 
+    // register any events
     void pollEvents();
 
     // Accessors
