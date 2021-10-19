a2ce0fb9568e61463721bd6af4f37909d5c95e23
FacuMH <facumolina15@gmail.com>
2021-10-19 14:16:06 +0200

    first experiment with sprites. added some assets for a character

--- include/Game.h
+++ include/Game.h
@@ -7,6 +7,15 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
 
+struct Position{
+    int x=0;
+    int y=0;
+    int x_offset=0;
+    int y_offset=0;
+    Position() : x(0), y(0), x_offset(0), y_offset(0){}
+    Position(int x, int y, int x_offset, int y_offset) : x(x), y(y), x_offset(x_offset), y_offset(y_offset) {}
+};
+
 /*
  * Wrapper class acting as game engine.
  */
@@ -20,6 +29,9 @@ private:
     sf::RenderWindow *window; // dynamically allocated so that it can be deleted
     sf::VideoMode videoMode;
     sf::Event event;
+    sf::Sprite character_sprite;
+    Position character_position;
+    sf::Texture character_texture;
 public:
     // Constructor
     Game();
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,17 +1,45 @@
+#include <iostream>
+
 #include "Game.h"
+
 
 // Private functions
 void Game::initVariables()
 {
     this->window = nullptr;
+    // this->character_sprite= nullptr;
+    // this->character_position= nullptr;
+    // this->character_texture= nullptr;
 }
 
 void Game::initWindow()
 {
-    this->videoMode.height = 480;
-    this->videoMode.width = 640;
+    this->videoMode.height = 720;
+    this->videoMode.width = 1280;
 
     this->window = new sf::RenderWindow(this->videoMode, "game", sf::Style::Titlebar | sf::Style::Close);
+
+    // character_sprite = new sf::Sprite();
+    // character_position = new Position(60,40,40,60);
+    // character_texture= new sf::Texture;
+
+    if (!character_texture.loadFromFile("../assets/character/Idle.png")){
+        std::cout << "character not found in: " <<  "assets/character/Idle.png" << "\n";
+    }
+    character_texture.setSmooth(true);
+    character_sprite.setTexture(character_texture);
+    // character_position->x=60;
+    // character_position->y=40;
+    // character_position->x_offset=40;
+    // character_position->y_offset=60;
+    character_position.x=65;
+    character_position.y=55;
+    character_position.x_offset=40;
+    character_position.y_offset=45;
+    // character_sprite.setTextureRect(sf::IntRect(character_position->x, character_position->y,
+    //                             character_position->x_offset, character_position->y_offset));
+        character_sprite.setTextureRect(sf::IntRect(character_position.x, character_position.y,
+                                character_position.x_offset, character_position.y_offset));
 }
 
 // Constructor
@@ -34,7 +62,6 @@ bool Game::isRunning() const
 }
 
 // Functions
-
 void Game::pollEvents()
 {
 // Event polling
@@ -53,6 +80,15 @@ void Game::pollEvents()
                     window->close();
                 }
                 break;
+            default:
+                //character_position->x= (character_position->x+160)%1620;
+                character_position.x= (character_position.x+165)%1620;
+                // character_sprite.setTextureRect(sf::IntRect(character_position->x, character_position->y,
+                //                             character_position->x_offset, character_position->y_offset));
+                character_sprite.setTextureRect(sf::IntRect(character_position.x, character_position.y,
+                                character_position.x_offset, character_position.y_offset));
+                //character_sprite.setPosition()
+                break;
         }
     }
 }
@@ -68,6 +104,7 @@ void Game::render()
     window->clear();
 
     // Draw game
+    window->draw(character_sprite);
 
     window->display();
     // Window is done drawing
