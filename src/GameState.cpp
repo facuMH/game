380b8ec4e9733eedfec23de5ecb102eef2bfed80
andrea <andrea.portscher@student.uibk.ac.at>
2021-11-17 12:40:39 +0100

    change imports order

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -1,26 +1,33 @@
+#include <iostream>
 #include "GameState.h"
 
-#include <iostream>
 
-GameState::GameState(sf::RenderWindow *window) : State(window) {}
+GameState::GameState(sf::RenderWindow *window) : State(window)
+{}
 
 GameState::~GameState() = default;
 
-void GameState::update(const float &dt) {
-  this->updateKeybinds(dt);
-  if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-    std::cout << "A pressed" << std::endl;
-  }
+void GameState::update(const float &dt)
+{
+    this->updateKeybinds(dt);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+    {
+        std::cout << "A pressed" << std::endl;
+    }
 }
 
-void GameState::render(sf::RenderTarget *target) {
+void GameState::render(sf::RenderTarget *target)
+{
     map.render(*target);
 }
 
-void GameState::checkIfQuitting() { State::checkIfQuitting(); }
+void GameState::checkIfQuitting()
+{ State::checkIfQuitting(); }
 
-void GameState::updateKeybinds(const float &dt) { this->checkIfQuitting(); }
+void GameState::updateKeybinds(const float &dt)
+{ this->checkIfQuitting(); }
 
-void GameState::quitStateActions() {
-  std::cout << "Ending current game state" << std::endl;
+void GameState::quitStateActions()
+{
+    std::cout << "Ending current game state" << std::endl;
 }
