141ec990eeccffd41b5e692876d391339ffcacbd
Facu MH <facumolina15@gmail.com>
2021-11-18 21:46:27 +0100

    templetize assetsManager loader

--- include/AssetsManager.h
+++ include/AssetsManager.h
@@ -1,24 +1,45 @@
 #pragma once
 
-#include <unordered_map>
 #include <string>
+#include <unordered_map>
 
 #include <SFML/Graphics.hpp>
 
 #include "definitions.h"
 
 class AssetsManager {
-private:
-  std::unordered_map<std::string, sf::Texture> textures;
-  std::unordered_map<std::string, sf::Font> fonts;
-  std::unordered_map<std::string, MapBackground> maps;
+  private:
+	std::unordered_map<std::string, sf::Texture> textures;
+	std::unordered_map<std::string, sf::Font> fonts;
+	std::unordered_map<std::string, MapBackground> maps;
 
-public:
-  sf::Texture getTexture(const std::string &name);
-  sf::Font getFont(const std::string &name);
-  MapBackground getMap(const std::string &name);
+	template <typename Asset, std::enable_if_t<std::is_same_v<Asset, sf::Texture>, bool> = true>
+	void emplace(Asset& newAsset) {
+		textures.emplace(newAsset);
+	}
 
-  void loadTexture(const std::string &path, const std::string &name);
-  void loadFont(const std::string &path, const std::string &name);
-  void loadMap(const std::string &path, const std::string &name);
+	template <typename Asset, std::enable_if_t<std::is_same_v<Asset, sf::Font>, bool> = true>
+	void emplace(Asset& newAsset) {
+		fonts.emplace(newAsset);
+	}
+
+	template <typename Asset, std::enable_if_t<std::is_same_v<Asset, MapBackground>, bool> = true>
+	void emplace(Asset& newAsset) {
+		maps.emplace(newAsset);
+	}
+
+  public:
+	sf::Texture getTexture(const std::string& name) { return maps.at(name); }
+	sf::Font getFont(const std::string& name) { return fonts.at(name); }
+	MapBackground getMap(const std::string& name) { return textures.at(name); }
+
+	template <typename Asset>
+	void loadAsset(const std::string& path, const std::string& name) {
+		Asset newAsset;
+		if(newAsset.loadFromFile(path)) {
+			emplace<Asset>(&newAsset);
+		} else {
+			std::cout << "RPG ERROR: no " << decltype(Asset) << " found at " << path << "\n";
+		}
+	}
 };
